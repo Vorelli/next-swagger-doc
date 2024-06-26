216d414838bf0272ccd939d43b9c4eaa7a147714
Natale Walter Toscano <46127615+Vorelli@users.noreply.github.com>
2024-06-26 02:53:19 -0600

    Try some stuff

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,4 +1,4 @@
-import traverse from "@babel/traverse";
+import * as traverse from "@babel/traverse";
 import * as doctrine from "doctrine";
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { join } from "path";
@@ -71,7 +71,7 @@ export function createSwaggerSpec({
 
   Object.entries(routesPathToCode).forEach(([route, code]) => {
     const ast = codeToAst(code);
-    traverse(ast, {
+    traverse.default(ast, {
       enter(path) {
         if (path.node.leadingComments) {
           for (const comment of path.node.leadingComments) {
